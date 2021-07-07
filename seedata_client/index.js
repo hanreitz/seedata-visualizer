fff5d1f852177971ff4944cedc1a0bbb0300aaa8
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-07-06 23:38:06 -0400

    added menu popup and background blur

--- seedata_client/index.js
+++ seedata_client/index.js
@@ -17,9 +17,15 @@ menuHolder.addEventListener('mouseover', handleMenuMouseover)
 menuHolder.addEventListener('mouseout', handleMenuMouseout)
 
 // listening for menu clicks
-menuContainer.addEventListener('click', handleMenuClick)
+menuContainer.addEventListener('click', toggleBlur)
 
 // handling menu events
+function toggleBlur(){
+  const blur = document.getElementById('blur')
+  blur.classList.toggle('active')
+  // const popup = document.getElementById('popup')
+  // popup.classList.toggle('active')
+}
 function handleMenuMouseover(){
   menuContainer.style.display = "block"
 }
@@ -28,21 +34,23 @@ function handleMenuMouseout(){
   menuContainer.style.display = "none"
 }
 
-function handleMenuClick(){
-  if (event.target.id === "menu-item-add-new-dataset"){
-    if (Dataset.datasetForm.style.display === "none"){
-      Dataset.datasetForm.style.display = "block"
-    } else {
-      Dataset.datasetForm.style.display = "none"
-    }
-  } else if (event.target.id === "menu-item-create-new-visualization"){
-    if (Visualization.visualizationForm.style.display === "none"){
-      Visualization.visualizationForm.style.display = "block"
-    } else {
-      Visualization.visualizationForm.style.display = "none"
-    }
-  }
-}
+// function handleMenuClick(){
+//   if (event.target.id === "menu-item-add-new-dataset"){
+//     if (Dataset.datasetForm.style.display === "none"){
+//       Dataset.datasetForm.style.display = "block"
+//       this.toggleBlur()
+//     } else {
+//       Dataset.datasetForm.style.display = "none"
+//     }
+//   } else if (event.target.id === "menu-item-create-new-visualization"){
+//     if (Visualization.visualizationForm.style.display === "none"){
+//       Visualization.visualizationForm.style.display = "block"
+//       this.toggleBlur()
+//     } else {
+//       Visualization.visualizationForm.style.display = "none"
+//     }
+//   }
+// }
 
 // render forms
 Dataset.renderForm()
@@ -58,6 +66,7 @@ function handleDatasetSubmit(){
   event.preventDefault()
   datasetService.createDataset()
   event.target.reset()
+  this.toggleBlur()
 }
 
 function handleVisualizationSubmit(){
@@ -76,8 +85,7 @@ function handleVisSpecSubmit(){
 // function for creating menu items
 
 function addMenuItem(item){
-  const menuItem = document.createElement('div')
-  menuItem.class = "menu-items"
+  const menuItem = document.createElement('p')
   menuItem.id = `menu-item-${item.toLowerCase().split(' ').join('-')}`
   menuItem.innerText = item
   menuContainer.append(menuItem)
